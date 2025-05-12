0c46354e86af32ecee81bfaa578e3e35283240b2
Appy-arsnel <75736454+Appy-arsnel@users.noreply.github.com>
2025-05-12 21:26:28 +0530

    Added Backend url

--- src/components/Recorder.jsx
+++ src/components/Recorder.jsx
@@ -10,8 +10,8 @@ export default function Recorder({ onResult }) {
   const streamRef = useRef(null);
   const audioContextRef = useRef(null);
 
-  const backendUrl = 'http://127.0.0.1:8000/classify';
-  // const backendUrl = 'https://ai-accent-backend.onrender.com/classify';
+  // const backendUrl = 'http://127.0.0.1:8000/classify';
+  const backendUrl = 'https://ai-accent-backend.onrender.com/classify';
 
   // Convert WebM/audio blob to WAV
   const convertToWav = async (blob) => {
